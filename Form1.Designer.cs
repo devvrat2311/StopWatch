ef9ac540cdc76fe32881e67764437c2276806b2c
daboof <debabrata1729@gmail.com>
2023-05-29 22:54:49 +0530

    ui changes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,22 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StopWatch));
             Start_bttn = new Button();
             Reset_bttn = new Button();
             Stop_bttn = new Button();
             label1 = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
             SuspendLayout();
             // 
             // Start_bttn
             // 
-            Start_bttn.BackgroundImage = (Image)resources.GetObject("Start_bttn.BackgroundImage");
-            Start_bttn.Font = new Font("Unispace", 13.7999992F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            Start_bttn.ForeColor = Color.Cyan;
-            Start_bttn.Location = new Point(12, 378);
+            Start_bttn.Font = new Font("Felix Titling", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Start_bttn.ForeColor = Color.Black;
+            Start_bttn.Location = new Point(1, 378);
             Start_bttn.Margin = new Padding(3, 4, 3, 4);
             Start_bttn.Name = "Start_bttn";
-            Start_bttn.Size = new Size(202, 85);
+            Start_bttn.Size = new Size(249, 85);
             Start_bttn.TabIndex = 0;
             Start_bttn.Text = "Start";
             Start_bttn.UseVisualStyleBackColor = true;
@@ -53,11 +52,11 @@
             // 
             // Reset_bttn
             // 
-            Reset_bttn.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
-            Reset_bttn.Location = new Point(549, 378);
+            Reset_bttn.Font = new Font("Felix Titling", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Reset_bttn.Location = new Point(551, 378);
             Reset_bttn.Margin = new Padding(3, 4, 3, 4);
             Reset_bttn.Name = "Reset_bttn";
-            Reset_bttn.Size = new Size(198, 85);
+            Reset_bttn.Size = new Size(258, 85);
             Reset_bttn.TabIndex = 1;
             Reset_bttn.Text = "Reset";
             Reset_bttn.UseVisualStyleBackColor = true;
@@ -65,11 +64,11 @@
             // 
             // Stop_bttn
             // 
-            Stop_bttn.Font = new Font("Unispace", 16.1999989F, FontStyle.Bold, GraphicsUnit.Point);
-            Stop_bttn.Location = new Point(267, 378);
+            Stop_bttn.Font = new Font("Felix Titling", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Stop_bttn.Location = new Point(256, 378);
             Stop_bttn.Margin = new Padding(3, 4, 3, 4);
             Stop_bttn.Name = "Stop_bttn";
-            Stop_bttn.Size = new Size(232, 85);
+            Stop_bttn.Size = new Size(289, 85);
             Stop_bttn.TabIndex = 2;
             Stop_bttn.Text = "Stop";
             Stop_bttn.UseVisualStyleBackColor = true;
@@ -77,10 +76,11 @@
             // 
             // label1
             // 
-            label1.Font = new Font("Yu Gothic", 90F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(12, 45);
+            label1.BackColor = Color.SpringGreen;
+            label1.Font = new Font("Felix Titling", 90F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 79);
             label1.Name = "label1";
-            label1.Size = new Size(770, 199);
+            label1.Size = new Size(770, 164);
             label1.TabIndex = 3;
             label1.Text = "00:00.00";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -89,13 +89,23 @@
             // 
             timer1.Tick += timer1_Tick;
             // 
+            // label2
+            // 
+            label2.Font = new Font("Felix Titling", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(1, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(236, 58);
+            label2.TabIndex = 4;
+            label2.Text = "Stopwatch.";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
             // StopWatch
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 128, 0);
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackColor = Color.SpringGreen;
             ClientSize = new Size(811, 600);
+            Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(Stop_bttn);
             Controls.Add(Reset_bttn);
@@ -118,5 +128,6 @@
         private Button Stop_bttn;
         private Label label1;
         private System.Windows.Forms.Timer timer1;
+        private Label label2;
     }
 }
